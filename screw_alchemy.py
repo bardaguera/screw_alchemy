88bffaa7f0e11c0051fccf890b6f427b6152ce38
Marya <bardaguera@gmail.com>
2020-11-02 20:06:47 +0300

    Update screw_alchemy.py

--- screw_alchemy.py
+++ screw_alchemy.py
@@ -2,6 +2,8 @@
 # coding: utf-8
 
 ##
+## Version 7 -- 2020-05-13
+#    Review after debugging
 ## Version 6 -- 2020-02-07
 #    Added method add_column to base class and to table attr
 ## Version 5 -- 2020-02-01
@@ -31,10 +33,12 @@ def alchemyencoder(obj):
         return float(obj)
     
 def jsonify_query_result(conn, query):
+    """deprecated. use instead."""
     res = query.all()
     #res = conn.execute(query)
     #return [dict(r) for r in res]
     return [r._asdict() for r in res]
+
 
 def transfer_columns(srs_meta, dest_table_obj):
     for column in srs_meta.columns:
@@ -46,11 +50,53 @@ def transfer_columns(srs_meta, dest_table_obj):
     return dest_table_obj
 
 
-def get_columns(self):
+def get_columns(self, mode='general'):
+    '''
+    :mode
+        'general'
+            Desc: wrapper for .__table__.columns sqlalchemy method
+            Out: [{'name': 'id', 'type': BIGINT(), 'nullable': False} ...]
+        'full'
+            Desc: format suitable for add_table, add_column methods
+            Out: []
+        'names-only'
+            Desc: list of column names
+            Out: ['id', ...]
+        'columns'
+            Desc: list of columns
+            Out: [Column('id', INTEGER(), table=<some_table>), ...]
+    '''
     col_list = []
-    for col in self.__table__.columns:
-        col_list.append({'name':col.name, 'type':col.type, 'nullable': col.nullable})
+    if mode == 'columns':
+         col_list = self.__table__.columns
+    else:
+        for col in self.__table__.columns:
+            col_list.append({'name':col.name, 'type':col.type, 'nullable': col.nullable})
+
+    if mode == 'names-only':
+        col_list = [c['name'] for c in col_list]
+    elif mode == 'full':
+        keys = self.get_keys_columns()
+        for d in col_list:
+            d['col_name'] = d['name']
+            d['col_type'] = d['type']
+            if d['name'] in keys:
+                d['is_primary'] = True
+            else:
+                d['is_primary'] = False
+
+            del d['name']
+            del d['type']
+
     return col_list
+
+def get_keys_columns(self):
+    return self._mapped_keys_
+
+def get_schema(self):
+    '''table function'''
+    return {'schema_name': self.__dict__['__table__'].schema,
+            'schema_obj': self.__dict__['__table__'].metadata}
 
 def _MArgsSerialize(m_args):
     if '__table__.c' in m_args:
@@ -82,6 +128,8 @@ def _UpdInstanceTypeMapping(engine, s):
                     'varchar': sqlalchemy.types.TEXT,
                     'char': sqlalchemy.types.TEXT,
                     'text': sqlalchemy.types.TEXT,
+                    'jsonb': {'postgresql':sqlalchemy.dialects.postgresql.JSON,
+                            'default':sqlalchemy.types.JSON},
                     'inet':  sqlalchemy.types.TEXT,
                     'float4': sqlalchemy.types.Float(precision=4),
                     'float8': sqlalchemy.types.Float(precision=8),
@@ -97,8 +145,10 @@ def _UpdInstanceTypeMapping(engine, s):
     else:
         return mapping_dict.get(s, False)
     
-def _UpdInstanceAddColumn(self, col_dict, table_name = None):
-    #Two use-cases: self is base_object or self is table_obj
+def _UpdInstanceAddColumn(self, col_dict, table_name = None, schema_name = None):    
+    '''Two use-cases: self is a base_object or self is a table_obj'''
+    if schema_name:
+        self.set_cur_schema(schema_name)
     if 'BaseInstance' in str(self.__class__):
         loc_engine_obj = self.engine
         if not table_name:
@@ -108,16 +158,30 @@ def _UpdInstanceAddColumn(self, col_dict, table_name = None):
     else:
         loc_table_name = self.__tablename__
         loc_engine_obj = self._engine_link_
+        if not schema_name:
+            loc_schema_name = self.get_schema()['schema_name']
 
-    if not _UpdInstanceTypeMapping(loc_engine_obj, col_dict['col_type']):
-        print('TYPE DOES NOT FOUND %s' % col_dict['col_type'])
-        return None
-    column_obj = Column(col_dict['col_name'],
-                        _UpdInstanceTypeMapping(loc_engine_obj, col_dict['col_type']),
-                        nullable=True)
-    loc_engine_obj.execute('ALTER TABLE {} ADD COLUMN {} {};'.format(loc_table_name,
-                                                                     column_obj.compile(dialect=loc_engine_obj.dialect),
-                                                                     column_obj.type.compile(loc_engine_obj.dialect)))
+    col_type = col_dict['col_type']
+
+    if type(col_type) is str:
+        if not _UpdInstanceTypeMapping(loc_engine_obj, col_type):
+            print('TYPE DOES NOT FOUND %s' % col_type)
+            return None
+        col_type = _UpdInstanceTypeMapping(loc_engine_obj, col_type)
+
+    column_obj = Column(col_dict['col_name'], col_type, nullable=True)
+    qs='ALTER TABLE {}."{}" ADD COLUMN {} {};'.format(self._cur_schema_.schema, #Do not add a quotes over column name
+                                                      loc_table_name,
+                                                      column_obj.compile(dialect=loc_engine_obj.dialect),
+                                                      column_obj.type.compile(loc_engine_obj.dialect))
+    try:
+        loc_engine_obj.execute(qs)
+    except SQLAlchemyError as se:
+        #TODO: поддержать логирование.
+        print('AZAZA', se)
+    else:
+        self._cur_schema_.reflect(only=[loc_table_name], extend_existing=True)
+    
 
 class BaseInstance(object):
     def __init__(self, instance = None, js = None):
@@ -140,7 +204,8 @@ class BaseInstance(object):
         setattr(self,
                 schema_name + '_meta',
                 MetaData(bind=self.engine, schema=schema_name))
-        self._cur_schema_ = eval('self.'+schema_name + '_meta')
+        self.set_cur_schema(schema_name)
+
         try:
             if type(schema_objects) != bool:
                 self._cur_schema_.reflect(only=schema_objects)
@@ -163,14 +228,18 @@ class BaseInstance(object):
                                       table_dict
                                       ))
         except ArgumentError as e:
+            #print('TABLE WTHT PK ',schema_obj.schema+'.'+table_name)
             table_data = schema_obj.tables[schema_obj.schema+'.'+table_name]
-            all_cols = list(c.name for c in t.columns)
+            all_cols = list(c.name for c in table_data.columns)
             self._gen_table_(table_name = table_name, schema_obj =schema_obj, m_args = {'primary_key':all_cols})
-            
         else:
             self._cur_table_ = eval('self.'+table_name)
+            if m_args:
+                self._cur_table_._mapped_keys_ = m_args.get('primary_key',None)
             self._cur_table_._engine_link_ = self.engine
             self._cur_table_.get_columns = MethodType(get_columns, self._cur_table_)
+            self._cur_table_.get_keys_columns = MethodType(get_keys_columns, self._cur_table_)
+            self._cur_table_.get_schema = MethodType(get_schema, self._cur_table_)
             self._cur_table_.add_column = MethodType(_UpdInstanceAddColumn, self._cur_table_)
         
         #setattr(eval('self.'+table), 'get_columns', get_columns)
@@ -204,17 +273,52 @@ class BaseInstance(object):
     def dispose(self):
         self.engine.dispose()
         
-    def add_column(self, col_dict, table_name):
-        _UpdInstanceAddColumn(self, col_dict, table_name = table_name)
+    def add_column(self, col_dict, table_name, schema_name):
+        """Add column to database by table name.
+        add_column(col_dict = {'col_name': 'surname', 'col_type': 'varchar'}, table_name = 'employee', schema_name='public')"""
+        _UpdInstanceAddColumn(self, col_dict, table_name = table_name, schema_name=schema_name)
 
-
-    def _UpdInstanceAddTable(self, table_name, columns_list, schema_obj = None):
+    def add_table(self, table_name, columns_list, prefixes = [], postfixes = [], schema_obj = None, recreate=False):
+        """Add table with columns to database, update schema and reflect this table to Base instance.
+        Base.create_table(table_name, columns_list, schema_obj = None)
+        By default table would be add into last used schema.
+        :table_name str
+        :columns_list columns_list [{'col_name': 'created_on', 'col_type': 'timestamp', 'is_primary': False}]
+        prefixes = [], postfixes = [], schema_obj = None, recreate=False
+        """
         if not schema_obj:
             schema_obj = self._cur_schema_
-        self.engine.execute('CREATE TABLE {}.{} ()'.format(schema_obj.schema, table_name))
+
+        if recreate:
+            self.engine.execute('DROP TABLE IF EXISTS {}."{}";'.format(schema_obj.schema, table_name))
+            if hasattr(self, table_name):
+                delattr(self, table_name)
+            
+        self.engine.execute('CREATE {} TABLE {}."{}" () {};'.format(' '.join(prefixes), schema_obj.schema, table_name,' '.join(postfixes)))
         for col_dict in columns_list:
             self.add_column(col_dict={'col_name': col_dict['col_name'], 'col_type': col_dict['col_type']},
-                            table_name=table_name)
-        schema_obj.reflect()
+                            table_name=table_name, schema_name=schema_obj.schema)
+        schema_obj.reflect(only=[table_name], extend_existing=True)
         key_names = list(d['col_name'] for d in filter(lambda d: d['is_primary'] == True, columns_list))
         self._gen_table_(table_name, schema_obj, {'primary_key': key_names})
+    
+    def reflect_table(self, table_name, schema_obj = None, m_args = None):
+         self._gen_table_(table_name=table_name, schema_obj = schema_obj, m_args = m_args)
+
+    def set_cur_schema(self, target_schema_name):
+        self._cur_schema_ = eval('self.'+target_schema_name + '_meta')
+        
+    def whoami(self):
+        print(self.engine.engine)
+
+    def fetch_to_dicts(self, query_obj):
+        '''return [status, [dict(), dict()], If error status == False'''
+        try:
+            res = self.engine.execute(query_obj)
+        except SQLAlchemyError as e:
+            return [False, e]
+        rows = res.fetchall()
+        query_cols = [str(c) for c in query_obj.columns]
+
+        #return [dict(r) for r in res]
+        return [True, [dict(zip(query_cols, r)) for r in rows]]
